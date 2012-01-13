8caa146fa9f2c1d18db26a1bee0e1a7b26d52f45
Tarek Ziadé <tarek@mozilla.com>
2012-01-13 15:17:59 -0800

    catch the sigint

--- powerhose/workers.cpp
+++ powerhose/workers.cpp
@@ -1,31 +1,48 @@
 #include <string.h>
 #include <zmq.hpp>
+#include <iostream>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace zmq;
 using namespace std;
 
+void bye(int param) {
+  cout << "Bye" << endl;
+  // cleanup ?
+  exit(1);
+}
+
+
 int main(int argc, const char* const argv[])
 {
+  signal(SIGINT, bye);
   static const char* const WORK = "ipc:///tmp/sender" ;
   static const char* const RES = "ipc:///tmp/receiver" ;
   static const char* const CTR = "ipc:///tmp/controller" ;
 
+  cout << "Building a context" << endl;
   context_t ctx(1);
 
   // channel to receive work
+  cout << "Starting the receiver channel" << endl;
   socket_t receiver(ctx, ZMQ_PULL);
   receiver.connect(WORK);
 
   // channel to send back results
+  cout << "Starting the result channel" << endl;
   socket_t sender(ctx, ZMQ_PUSH) ;
   sender.connect(RES);
 
   // channel for the controller
+  cout << "Starting the control channel" << endl;
   socket_t control(ctx, ZMQ_SUB) ;
   control.connect(CTR);
 
   // Set up a poller to multiplex the work receiver and
   // control receiver channels
+  cout << "Starting the poller" << endl;
   zmq_pollitem_t items[2];
   items[0].socket = receiver;
   items[0].events = ZMQ_POLLIN;
@@ -33,6 +50,8 @@ int main(int argc, const char* const argv[])
   items[1].events = ZMQ_POLLIN;
 
   // now loop and accept messages from the poller
+  cout << "Looping now..." << endl;
+
   while (true) {
     poll(items, 2, -1);
 
@@ -56,8 +75,5 @@ int main(int argc, const char* const argv[])
         // do something with the message
     }
   }
-
-  return 0 ;
+  return 0;
 }
-
-
